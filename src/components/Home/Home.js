35c85df87e71530e0eabb410c809e87c37f3432f
DEV-1003\Akbar <Akbar.Hossain@symphonysoftt.com>
2025-07-15 13:35:47 +0600

    Name Change

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -22,7 +22,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> AKbar</strong>
+                <strong className="main-name">Md. Akbar Hossain</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
